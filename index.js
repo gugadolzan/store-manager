6dde86ee3ad6a63366b136e2520a3f6657151a4e
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-28 14:34:41 -0300

    Setup environment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+  // console.log(`Escutando na porta ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
