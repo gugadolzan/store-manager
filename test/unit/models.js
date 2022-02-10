ece52e0c9cb90f5357675aea768cec6a877f2d2d
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-10 14:48:17 -0300

    Implement tests for productsModel getByName method

--- test/unit/models.js
+++ test/unit/models.js
@@ -5,10 +5,10 @@ const connection = require("../../models/connection");
 const productsModel = require("../../models/productsModel.js");
 
 describe("Call the create method from the productsModel", () => {
-  const newProduct = { name: "produto", quantity: 10 };
+  const product = { name: "produto", quantity: 10 };
 
   before(() => {
-    const execute = [{ ...newProduct, insertId: 1 }];
+    const execute = [{ ...product, insertId: 1 }];
 
     sinon.stub(connection, "execute").resolves(execute);
   });
@@ -19,15 +19,63 @@ describe("Call the create method from the productsModel", () => {
 
   describe("when the product is created", () => {
     it("should return an object", async () => {
-      const response = await productsModel.create(newProduct);
+      const response = await productsModel.create(product);
 
       expect(response).to.be.an("object");
     });
 
-    it("should return an object with the properties 'id', 'name' and 'quantity'", async () => {
-      const response = await productsModel.create(newProduct);
+    it("should return an object with the properties 'id', 'name' and 'quantity' and respective values", async () => {
+      const response = await productsModel.create(product);
 
       expect(response).to.include.all.keys("id", "name", "quantity");
+      expect(response).to.deep.equal({ ...product, id: 1 });
+    });
+  });
+});
+
+describe("Call the getByName method from the productsModel", () => {
+  describe("when the product already exists", () => {
+    const product = { name: "produto", quantity: 100 };
+
+    before(() => {
+      const execute = [[{ ...product, id: 1 }]];
+
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it("should return an object", async () => {
+      const response = await productsModel.getByName({ name: "produto" });
+
+      expect(response).to.be.an("object");
+    });
+
+    it("should return an object with the properties 'id', 'name' and 'quantity' and respective values", async () => {
+      const response = await productsModel.getByName(product);
+
+      expect(response).to.include.all.keys("id", "name", "quantity");
+      expect(response).to.deep.equal({ ...product, id: 1 });
+    });
+  });
+
+  describe("when the product does not exist", () => {
+    const product = { name: "produto", quantity: 10 };
+
+    before(() => {
+      sinon.stub(connection, "execute").resolves([[]]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it("should return undefined", async () => {
+      const response = await productsModel.getByName(product);
+
+      expect(response).to.be.undefined;
     });
   });
 });
