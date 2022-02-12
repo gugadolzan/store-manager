771cb06bb7cef6e447f4bc1b50ffed79ef4dafcc
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-12 00:56:15 -0300

    Centralize middlewares in controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,26 +1,26 @@
 const rescue = require('express-rescue');
 
-const validateProduct = require('./middlewares/validateProduct');
 const productsService = require('../services/productsService');
-
+const productsSchema = require('../schemas/productsSchema');
 const { HTTP_STATUS_CODES } = require('../utils/statusCodes');
 
-const create = [
-  validateProduct,
-  rescue(async (req, res) => {
-    const { name, quantity } = req.body;
+const create = rescue(async (req, res) => {
+  const { name, quantity } = req.body;
 
-    const product = await productsService.create({ name, quantity });
+  const { error } = productsSchema.validate({ name, quantity });
 
-    if (product.error) {
-      const err = new Error(product.error.message);
-      err.code = product.error.code;
-      throw err;
-    }
+  if (error) throw error;
 
-    res.status(HTTP_STATUS_CODES.CREATED).json(product);
-  }),
-];
+  const product = await productsService.create({ name, quantity });
+
+  if (product.error) {
+    const err = new Error(product.error.message);
+    err.code = product.error.code;
+    throw err;
+  }
+
+  res.status(HTTP_STATUS_CODES.CREATED).json(product);
+});
 
 const getAll = rescue(async (_req, res) => {
   const products = await productsService.getAll();
@@ -42,23 +42,24 @@ const getById = rescue(async (req, res) => {
   res.status(HTTP_STATUS_CODES.OK).json(product);
 });
 
-const update = [
-  validateProduct,
-  rescue(async (req, res) => {
-    const { name, quantity } = req.body;
-    const { id } = req.params;
+const update = rescue(async (req, res) => {
+  const { name, quantity } = req.body;
+  const { id } = req.params;
 
-    const product = await productsService.update({ id, name, quantity });
+  const { error } = productsSchema.validate({ name, quantity });
 
-    if (product.error) {
-      const err = new Error(product.error.message);
-      err.code = product.error.code;
-      throw err;
-    }
+  if (error) throw error;
 
-    res.status(HTTP_STATUS_CODES.OK).json(product);
-  }),
-];
+  const product = await productsService.update({ id, name, quantity });
+
+  if (product.error) {
+    const err = new Error(product.error.message);
+    err.code = product.error.code;
+    throw err;
+  }
+
+  res.status(HTTP_STATUS_CODES.OK).json(product);
+});
 
 const remove = rescue(async (req, res) => {
   const { id } = req.params;
