dbaea0233b736cbba122b58cb67e59c5019eec84
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-13 18:19:26 -0300

    Implement sales service tests

--- test/unit/services.js
+++ test/unit/services.js
@@ -2,7 +2,9 @@ const { expect } = require("chai");
 const sinon = require("sinon");
 
 const productsModel = require("../../models/productsModel.js");
+const salesModel = require("../../models/salesModel.js");
 const productsService = require("../../services/productsService.js");
+const salesService = require("../../services/salesService.js");
 
 describe("Products service", () => {
   describe("when calling the create method", () => {
@@ -257,6 +259,270 @@ describe("Products service", () => {
         } catch (error) {
           expect(error.message).to.be.equal("productNotFound");
         }
+      });
+    });
+  });
+});
+
+describe("Sales service", () => {
+  describe("when calling the create method", () => {
+    describe("when the sales are created", () => {
+      const sales = [
+        {
+          product_id: 1,
+          quantity: 2,
+        },
+        {
+          product_id: 2,
+          quantity: 5,
+        },
+      ];
+
+      const result = {
+        id: 1,
+        itemsSold: [
+          {
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            product_id: 2,
+            quantity: 5,
+          },
+        ],
+      };
+
+      before(() => {
+        sinon
+          .stub(productsModel, "getById")
+          .onFirstCall()
+          .resolves({
+            id: 1,
+            name: "produto A",
+            quantity: 10,
+          })
+          .onSecondCall()
+          .resolves({
+            id: 2,
+            name: "produto B",
+            quantity: 20,
+          });
+        sinon.stub(productsModel, "update").resolves();
+        sinon.stub(salesModel, "create").resolves(result);
+      });
+
+      after(() => {
+        productsModel.getById.restore();
+        productsModel.update.restore();
+        salesModel.create.restore();
+      });
+
+      it("should return an object with the properties 'id', 'product' and 'quantity' and respective values", async () => {
+        const response = await salesService.create(sales);
+
+        expect(response).to.be.an("object");
+        expect(response).to.include.all.keys("id", "itemsSold");
+        expect(response).to.deep.equal({
+          id: 1,
+          itemsSold: [
+            {
+              product_id: 1,
+              quantity: 2,
+            },
+            {
+              product_id: 2,
+              quantity: 5,
+            },
+          ],
+        });
+      });
+    });
+  });
+
+  describe("when calling the getAll method", () => {
+    describe("when there are sales", () => {
+      before(() => {
+        const sales = [
+          {
+            saleId: 1,
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            saleId: 1,
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ];
+
+        sinon.stub(salesModel, "getAll").resolves(sales);
+      });
+
+      after(() => {
+        salesModel.getAll.restore();
+      });
+
+      it("should return an array of objects and respective values", async () => {
+        const response = await salesService.getAll();
+
+        expect(response).to.be.an("array");
+        expect(response).to.deep.equal([
+          {
+            saleId: 1,
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            saleId: 1,
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ]);
+      });
+    });
+  });
+
+  describe("when calling the getById method", () => {
+    describe("when the sale exists", () => {
+      before(() => {
+        const sale = [
+          {
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ];
+
+        sinon.stub(salesModel, "getById").resolves(sale);
+      });
+
+      after(() => {
+        salesModel.getById.restore();
+      });
+
+      it("should return an array of objects and respective values", async () => {
+        const response = await salesService.getById({ id: 1 });
+
+        expect(response).to.be.an("array");
+        expect(response).to.deep.equal([
+          {
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ]);
+      });
+    });
+  });
+
+  describe("when calling the update method", () => {
+    describe("when the sale exists", () => {
+      before(() => {
+        const sale = [
+          {
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ];
+
+        sinon.stub(salesModel, "getById").resolves(sale);
+        sinon.stub(salesModel, "update").resolves();
+      });
+
+      after(() => {
+        salesModel.getById.restore();
+        salesModel.update.restore();
+      });
+
+      it("should return an object with the properties 'saleId' and 'itemUpdated'", async () => {
+        const response = await salesService.update({
+          id: 1,
+          sale: {
+            product_id: 1,
+            quantity: 6,
+          },
+        });
+
+        expect(response).to.be.an("object");
+        expect(response).to.include.all.keys("saleId", "itemUpdated");
+        expect(response).to.deep.equal({
+          saleId: 1,
+          itemUpdated: [
+            {
+              product_id: 1,
+              quantity: 6,
+            },
+          ],
+        });
+      });
+    });
+  });
+
+  describe("when calling the remove method", () => {
+    describe("when the sale exists", () => {
+      before(() => {
+        sinon.stub(salesModel, "getById").resolves([
+          {
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ]);
+        sinon
+          .stub(productsModel, "getById")
+          .resolves({ id: 1, name: "produto A", quantity: 10 });
+        sinon.stub(productsModel, "update").resolves();
+        sinon.stub(salesModel, "remove").resolves();
+      });
+
+      after(() => {
+        salesModel.getById.restore();
+        productsModel.getById.restore();
+        productsModel.update.restore();
+        salesModel.remove.restore();
+      });
+
+      it("should return an array of objects and respective values", async () => {
+        const response = await salesService.remove({ id: 1 });
+
+        expect(response).to.be.an("array");
+        expect(response).to.deep.equal([
+          {
+            date: "2021-09-09T04:54:29.000Z",
+            product_id: 1,
+            quantity: 2,
+          },
+          {
+            date: "2021-09-09T04:54:54.000Z",
+            product_id: 2,
+            quantity: 2,
+          },
+        ]);
       });
     });
   });
